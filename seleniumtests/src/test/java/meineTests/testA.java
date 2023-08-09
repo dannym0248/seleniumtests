c0131c73a0440b039f72a6d92617def353da8535
danny <danny@DESKTOP-RBK37QC>
2023-08-09 10:23:56 +0200

    test

--- seleniumtests/src/test/java/meineTests/testA.java
+++ seleniumtests/src/test/java/meineTests/testA.java
@@ -1,4 +1,4 @@
-package com.test;
+package meineTests;
 
 import java.io.File;
 import java.net.URL;
@@ -7,9 +7,8 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class testA {
@@ -17,15 +16,18 @@ public class testA {
 	@Test
 	public void test_aufruf_google() throws Exception {
 		URL url_webseite = new URL("https://www.google.com");
-		URL url_hub = new URL("192.168.16.121:4444");
-		FirefoxOptions options = new FirefoxOptions();
+		URL url_hub = new URL("http://192.168.16.121:4444/wd/hub");
+		
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("platformName", "linux");
 		
 		WebDriver driver = new RemoteWebDriver(url_hub, options);
+		driver.manage().window().maximize();
 		driver.get(url_webseite.toString());
 
-		File screenshot = new File(System.getProperty("user.dir") + "\\screen_01" + ".jpg");
-		testA.takeSnapShot(driver, screenshot);
-		Reporter.log("Screenshot at " + screenshot.getAbsolutePath(), true);
+//		File screenshot = new File(System.getProperty("user.dir") + "\\screen_01" + ".jpg");
+//		testA.takeSnapShot(driver, screenshot);
+//		Reporter.log("Screenshot at " + screenshot.getAbsolutePath(), true);
 		
 		driver.quit();
 	}
